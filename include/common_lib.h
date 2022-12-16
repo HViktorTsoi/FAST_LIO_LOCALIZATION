cdceeedb7289d575886f5482e8afe8379d60eb44
Marco Mollo <73255614+MarcoMollo@users.noreply.github.com>
2022-12-16 12:46:11 +0100

    Updated common_lib.h
    Error fast_lio/Pose6D.h: No such file or directory
    
    Solved by changing 
    typedef fast_lio::Pose6D Pose6D; 
    to
    typedef fast_lio_localization::Pose6D Pose6D;

--- include/common_lib.h
+++ include/common_lib.h
@@ -33,7 +33,7 @@ using namespace Eigen;
 #define STD_VEC_FROM_EIGEN(mat)  vector<decltype(mat)::Scalar> (mat.data(), mat.data() + mat.rows() * mat.cols())
 #define DEBUG_FILE_DIR(name)     (string(string(ROOT_DIR) + "Log/"+ name))
 
-typedef fast_lio::Pose6D Pose6D;
+typedef fast_lio_localization::Pose6D Pose6D;
 typedef pcl::PointXYZINormal PointType;
 typedef pcl::PointCloud<PointType> PointCloudXYZI;
 typedef vector<PointType, Eigen::aligned_allocator<PointType>>  PointVector;
